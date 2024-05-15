06a498444321e67e8a19b140c4e99ff9e4f0a619
felipe rodriguez <felipe.rodriguezri@gmail.com>
2024-05-15 12:25:40 -0400

    se crea nuevo repositorio para el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 :style="{ color: principal }">Titulo principal</h1>
+
+    <div>
+      <!-- cambio de Rango de tamaño imagen mendiante %  -->
+      <label for="rangeImg">Ancho Imagen</label>
+      <input type="range" id="rangeimg" name="vol" value="100" min="20" max="100" step="1" v-model="anchoImagen">
+      <span>{{ anchoImagen + '%' }}</span>
+
+      <!-- cambio de Rango de sombra mendiante %  -->
+      <label for="sombraImg">sombra imagen</label>
+      <input type="range" id="sombraimg" name="vol" value="100" min="20" max="100" step="1" v-model="sombra">
+
+      <!-- cambio de Rango de radius mendiante %  -->
+      <label for="radiusImg">border radius imagen</label>
+      <input type="range" id="radiusimg" name="vol" value="100" min="20" max="100" step="1" v-model="bordeRedondo">
+      <span>{{ anchoImagen + '%' }}</span>
+      <figure>
+        <img :width="anchoImagen + '%'" src="https://picsum.photos/id/1025/1200/800" alt="" :style="{
+          boxShadow: sombraImagen,
+          borderRadius: bordeRedondo + '%',
+        }" />
+      </figure>
+    </div>
+
+    <div style="padding: 25px 25px;">
+      <p :class="{ realizado: p1 }">Texto de ejemplo1 <input type="checkbox" name="" id="" v-model="p1"> </p>
+      <p :class="{ realizado: p2 }">Texto de ejemplo2 <input type="checkbox" name="" id="" v-model="p2"></p>
+      <p :class="{ realizado: p3 }">Texto de ejemplo3 <input type="checkbox" name="" id="" v-model="p3"></p>
+      <p :class="{ realizado: p4 }">Texto de ejemplo4 <input type="checkbox" name="" id="" v-model="p4"></p>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      principal: "blue",
+      anchoImagen: 100,
+      bordeRedondo: 0,
+      sombra: "50px 50px 50px 50px rgba(0, 0, 0, 0.75)",
+      p1: false,
+      p2: false,
+      p3: false,
+      p4: false,
+    }
+  },
+  computed: {
+    sombraImagen: function () {
+      let { sHorizontal, sVertical, sBlurRadius, sSpreadRadius, sColor } = this;
+      let boxShadow = `${sHorizontal}px ${sVertical}px ${sBlurRadius}px ${sSpreadRadius}px ${sColor}`;
+      return boxShadow;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.realizado {
+  color: red;
+  font-weight: 800;
+  font-size: 24px;
 }
 </style>
---
+++ src/AppProfe.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <h1 :style="{ color: principal }">Titulo principal</h1>
+
+    <div>
+      <div>
+        <label for="rangeWidth">Ancho imagen: </label>
+        <input type="range" id="rangeWidth" name="rangeWidth" min="30" max="100" step="1" v-model="anchoImagen">
+        <span>{{ anchoImagen + "%" }}</span>
+      </div>
+      <div>
+        <label for="rangeRadius">Border radius: </label>
+        <input type="range" id="rangeRadius" name="rangeRadius" min="0" max="50" step="1" v-model="radiusImagen">
+        <span>{{ radiusImagen + "%" }}</span>
+      </div>
+      <div>
+        <hr>
+        <span>Sombra horizontal: </span> <input type="range" id="rangeSombraHorizontal" name="rangeSombraHorizontal"
+          min="-20" max="30" step="1" v-model="sHorizontal"><span>{{ sHorizontal + "%" }} | </span>
+
+        <span>Sombra Vertical: </span> <input type="range" id="rangeSombraVertical" name="rangeSombraVertical" min="-20"
+          max="30" step="1" v-model="sVertical"><span>{{ sVertical + "%" }} | </span>
+
+        <span>Color sombra: </span> <input type="color" id="rangeSombraColor" name="rangeSombraColor"
+          v-model="sColor"><span> | </span>
+
+
+      </div>
+      <figure>
+        <img :width="anchoImagen + '%'" src="https://picsum.photos/id/1025/1200/800" alt="" :style="{
+          boxShadow: sombraImagen,
+          '-webkit-box-shadow': sombraImagen,
+          '-moz-box-shadow': sombraImagen,
+          borderRadius: radiusImagen + '%'
+        }" />
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      principal: "blue",
+      anchoImagen: 100,
+      radiusImagen: 0,
+      sHorizontal: 10,
+      sVertical: 10,
+      sBlurRadius: 20,
+      sSpreadRadius: 20,
+      sColor: "rgba(0,0,0,0.75",
+    };
+  },
+  computed: {
+    sombraImagen: function () {
+      let { sHorizontal, sVertical, sBlurRadius, sSpreadRadius, sColor } = this;
+      let boxShadow = `${sHorizontal}px ${sVertical}px ${sBlurRadius}px ${sSpreadRadius}px ${sColor}`;
+      return boxShadow;
+    }
+  }
+};
+</script>
+
+<style></style>
